3a28623302c1bebb8d9344ef0b4e0b0f9c5d8344
Zalessky Maxim <zalessky.maxim@gmail.com>
2015-10-27 14:13:33 +0300

    Add Interface IPunctuationMarkWorker

---
+++ Task2_1/Interfaces/IPunctuationMarkWorker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2_1.Model;
+
+namespace Task2_1.Interfaces
+{
+    public interface IPunctuationMarkWorker
+    {
+        bool IsQuestionMark(SentenceElement element);
+    }
+}
