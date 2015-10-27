48efbe74e598d8392954ea0c9f964af735763d30
Zalessky Maxim <zalessky.maxim@gmail.com>
2015-10-27 14:08:55 +0300

    Add interface ISentence

---
+++ Task2_1/Interfaces/ISentence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2_1.Model;
+
+namespace Task2_1.Interfaces
+{
+    public interface ISentence
+    {
+        void AddElementToEnd(SentenceElement element);
+        int GetWordsCount();
+        int GetElementsCount();
+        SentenceElement GetElementByIndex(int index);
+        void DeleteWords(int wordLenght);
+        void ReplaceWords(int wordLenght, string newValue);
+    }
+}
