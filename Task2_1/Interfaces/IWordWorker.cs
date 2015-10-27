917e0bab68e4dc1abf1f2a7f77ebd696ee8fab83
Zalessky Maxim <zalessky.maxim@gmail.com>
2015-10-27 14:11:30 +0300

    Add Interface IWordWorker

---
+++ Task2_1/Interfaces/IWordWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2_1.Model;
+
+namespace Task2_1.Interfaces
+{
+    public interface IWordWorker
+    {
+        int GetWordLength(SentenceElement element);
+        bool FirstLetterIsConsonant(SentenceElement element);
+        void ReplaceValue(int wordLenght, SentenceElement element, string newValue);
+    }
+}
